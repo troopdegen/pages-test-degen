ef6e0022c85392d208972c72609f71454465063a
Mel <angel.melendez.c@gmail.com>
2024-03-23 04:50:46 -0600

    update favicon meta tag

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from 'next/head'
 
 const Meta = () => (
   <Head>
@@ -6,10 +6,7 @@ const Meta = () => (
     <meta charSet="utf-8" />
     <meta name="mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta
-      name="apple-mobile-web-app-status-bar-style"
-      content="black-translucent"
-    />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
     <meta name="apple-mobile-web-app-title" content="Sr. Degen" />
     <meta name="application-name" content="Sr. Degen" />
     <meta name="theme-color" content="#4338ca" />
@@ -17,12 +14,9 @@ const Meta = () => (
       name="viewport"
       content="width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover"
     />
-    <link
-      rel="icon"
-      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎩</text></svg>"
-    />
+    <link rel="icon" href="/favicon.ico" sizes="any" />
     <link rel="manifest" href="/manifest.json" />
   </Head>
-);
+)
 
-export default Meta;
+export default Meta
